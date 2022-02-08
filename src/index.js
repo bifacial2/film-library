b1327809d81ef1725a9a7c830303dc81383804b8
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2022-02-08 14:07:58 +0200

    add-files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './js/render-films';
 import './js/modal-film-card';
 import './js/render-header';
 import './js/localization';
+import './js/toggle-theme';
 import './js/dictionary';
 import './js/find-film';
 import './js/watched-films';
